829137cdc83ad4ec2a6d71b4bb42b4b51d989d62
tramanh9999 <43420892+tramanh9999@users.noreply.github.com>
2020-04-14 23:08:35 +0700

    backup doc

--- micro-services/user-service/usDocument/roleInsert.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO storyart_db.role(`id`,`name`) VALUES (3,'ROLE_ADMIN');
-INSERT INTO storyart_db.role (`id`,`name`) VALUES (2,'ROLE_SYSTEM_ADMIN');
-INSERT INTO storyart_db.role(`id`,`name`) VALUES (1,'ROLE_USER');
-
-
