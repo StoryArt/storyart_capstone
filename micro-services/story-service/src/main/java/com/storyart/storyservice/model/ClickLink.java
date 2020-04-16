8704394b651cb6c078b391642060279249dad442
tramanh9999 <43420892+tramanh9999@users.noreply.github.com>
2020-04-16 19:19:34 +0700

    fix intergrity

--- micro-services/story-service/src/main/java/com/storyart/storyservice/model/ClickLink.java
+++ micro-services/story-service/src/main/java/com/storyart/storyservice/model/ClickLink.java
@@ -16,7 +16,7 @@ import java.util.Date;
 @AllArgsConstructor
 public class ClickLink {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private int storyId;
     private String link;
