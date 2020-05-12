6f41057e1fb9b1fea7213ccc028687a758148472
DATNT <tandat1405@gmail.com>
2020-05-12 15:46:22 +0700

    push

--- web-frontend/enduser/src/components/common/StoryGraph.js
+++ web-frontend/enduser/src/components/common/StoryGraph.js
@@ -3,12 +3,12 @@ import StoryService from '../../services/story.service';
 import Graph from "react-graph-vis";
 
 
-let options = { 
-    physics: { enabled: false, stabilization: { iterations: 5000 } }, 
+let options = {
+    physics: { enabled: false, stabilization: { iterations: 5000 } },
     clickToUse: true,
     layout: {
         randomSeed: undefined,
-        improvedLayout:true,
+        improvedLayout: true,
         clusterThreshold: 150,
         hierarchical: {
             enabled: true,
@@ -19,13 +19,13 @@ let options = {
             edgeMinimization: true,
             parentCentralization: true,
             direction: 'UD',        // UD, DU, LR, RL
-            sortMethod: 'hubsize',  // hubsize, directed
+            sortMethod: 'directed',  // hubsize, directed
             shakeTowards: 'leaves'  // roots, leaves
-        }, 
+        },
     },
-    nodes : { shape : 'square' }, 
+    nodes: { shape: 'square' },
     autoResize: false,
-    height: '500px', 
+    height: '600px',
     // physics: {
     //     forceAtlas2Based: {
     //         gravitationalConstant: -26,
@@ -43,15 +43,16 @@ let options = {
     //         updateInterval: 25
     //     }
     // },
-    edges: { 
-        smooth: false, 
-        color: '#000000', 
-        width: 0.5, 
+    edges: {
+        smooth: { type: "vertical", roundness: 0.2 },
+        color: '#000000',
+        width: 0.5,
         length: 400,
-        arrows: { 
-            to: { enabled: true, scaleFactor: 0.5 } 
-        } }, 
-    interaction: { hover: true, keyboard :false, navigationButtons: false, tooltipDelay: 150 } 
+        arrows: {
+            to: { enabled: true, scaleFactor: 0.5 }
+        }
+    },
+    interaction: { hover: true, keyboard: false, navigationButtons: false, tooltipDelay: 150 }
 };
 
 const StoryGraph = (props) => {
@@ -60,10 +61,10 @@ const StoryGraph = (props) => {
     const graph = { nodes, edges };
 
     const events = {
-        select: function(event) {
-          var { nodes, edges } = event;
+        select: function (event) {
+            var { nodes, edges } = event;
         },
-        selectNode: function(event){
+        selectNode: function (event) {
             var { nodes, edges } = event;
             setCurrentScreen(nodes[0]);
         }
