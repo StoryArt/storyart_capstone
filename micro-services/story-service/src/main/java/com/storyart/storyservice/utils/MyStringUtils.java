fd38c51f490679b17e722fac4fe69ef46d7eafb2
DATNT <tandat1405@gmail.com>
2020-05-12 14:44:36 +0700

    valid url

--- micro-services/story-service/src/main/java/com/storyart/storyservice/utils/MyStringUtils.java
+++ micro-services/story-service/src/main/java/com/storyart/storyservice/utils/MyStringUtils.java
@@ -1,7 +1,9 @@
 package com.storyart.storyservice.utils;
 
+import org.hibernate.validator.internal.constraintvalidators.hv.URLValidator;
 import org.springframework.util.StringUtils;
 
+import java.net.URL;
 import java.util.Random;
 import java.util.UUID;
 
@@ -17,9 +19,8 @@ public class MyStringUtils {
     }
 
     public static boolean isValidUrl(String url){
-        //if(StringUtils.isEmpty(url)) return false;
-        //String regex = "^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?([a-z]+)?$";
-        //return url.matches(regex);
-        return true;
+        if(StringUtils.isEmpty(url)) return false;
+        String regex = "^(http:\\/\\/|https:\\/\\/)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.?(.+)?$";
+        return url.matches(regex);
     }
 }
